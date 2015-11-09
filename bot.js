0b1eeaad1ebb7240364f7fe53fe3b75e3d508147
Phillip Smith <ps@phillipadsmith.com>
2015-11-09 09:06:34 -0800

    Re-implement CouchDB logging, also opts to sendMessage

--- bot.js
+++ bot.js
@@ -11,16 +11,31 @@ var bot = new TelegramBot(token, {
     polling: true
 });
 
+var
+  util = require('util'),
+  couchdb = require('felix-couchdb'),
+  client = couchdb.createClient(5984, 'localhost'),
+  db = client.db('bot-logging');
+
 console.log("The bot is starting...");
 plugins.runPlugins(config.activePlugins);
 
 bot.on('message', function(msg) {
     if (msg.text) {
+        // MVP for logging
+        // TODO replace with Winston, https://github.com/winstonjs/winston
         var chatId = msg.chat.id;
+        var messageId = msg.message_id;
+        db
+          .saveDoc(messageId, msg, function(er, ok) {
+            if (er) throw new Error(JSON.stringify(er));
+            console.log('Saved doc to couch:');
+            console.log(msg);
+          });
         plugins.doMessage(msg, function(reply) {
             switch (reply.type) {
                 case "text":
-                    bot.sendMessage(chatId, reply.text);
+                    bot.sendMessage(chatId, reply.text, reply.opts);
                     break;
                 case "audio":
                     bot.sendAudio(chatId, reply.audio);
