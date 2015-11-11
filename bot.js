7fff79e724ca8a4775213e50d28fe962d1de33e0
Phillip Smith <ps@phillipadsmith.com>
2015-11-10 21:21:20 -0800

    Basic logging of request and response to CouchDB

--- bot.js
+++ bot.js
@@ -12,52 +12,70 @@ var bot = new TelegramBot(token, {
 });
 
 var
-  util = require('util'),
-  couchdb = require('felix-couchdb'),
-  client = couchdb.createClient(5984, 'localhost'),
-  db = client.db('bot-logging');
+util = require('util'),
+    couchdb = require('felix-couchdb'),
+    client = couchdb.createClient(5984, 'localhost'),
+    db = client.db('bot-logging');
 
 console.log("The bot is starting...");
 plugins.runPlugins(config.activePlugins);
 
 bot.on('message', function(msg) {
     if (msg.text) {
-        // MVP for logging
         // TODO replace with Winston, https://github.com/winstonjs/winston
         var chatId = msg.chat.id;
-        var messageId = msg.message_id;
-        db
-          .saveDoc(messageId, msg, function(er, ok) {
-            if (er) throw new Error(JSON.stringify(er));
-            console.log('Saved doc to couch:');
-            console.log(msg);
-          });
+        logMsg(msg);
         plugins.doMessage(msg, function(reply) {
             switch (reply.type) {
                 case "text":
                     bot.sendMessage(chatId, reply.text, reply.opts);
-                    break;
+                break;
                 case "audio":
                     bot.sendAudio(chatId, reply.audio);
-                    break;
+                break;
                 case "photo":
                     bot.sendPhoto(chatId, reply.photo);
-                    break;
+                break;
                 case "status":
                     bot.sendChatAction(chatId, reply.status);
-                    break;
+                break;
                 default:
                     console.log("Error: Unrecognized response");
             }
+            logReply(msg, reply);
         });
     }
 });
+
+function logMsg(msg) {
+    db
+    .saveDoc(msg.message_id, msg, function(er, ok) {
+        if (er) throw new Error(JSON.stringify(er));
+    });
+}
+
+function logReply(msg, reply) {
+    db
+    .getDoc(msg.message_id, function(er, doc) {
+        if (er) throw new Error(JSON.stringify(er));
+        var newDoc = doc;
+        newDoc.reply  = reply.opts;
+        newDoc._rev = doc._rev;
+        updateDoc(newDoc);
+    });
+}
+
+function updateDoc(doc) {
+    db.saveDoc(doc.message_id, doc, function(er, ok) {
+        if (er) throw new Error(JSON.stringify(er));
+    });
+}
 
 // If `CTRL+C` is pressed we stop the bot safely.
 process.on('SIGINT', shutDown);
 
 // Stop safely in case of `uncaughtException`.
-process.on('uncaughtException', function(error) { 
+process.on('uncaughtException', function(error) {
     console.log(error);
     shutDown();
 });
