f6b78ab1085ac47d12944e499f7c7cfc75eca79e
Phillip Smith <ps@phillipadsmith.com>
2016-06-08 17:46:35 +0200

    Check for existance of properties. Closes #35

--- modules/beautifulrising/utils.js
+++ modules/beautifulrising/utils.js
@@ -1,3 +1,15 @@
 exports.test = function() {
     console.log('Running test...');
 };
+
+exports.checkForMore = function(module) {
+    if ( module['short-write-up'] !== undefined ) {
+        return module;
+    }
+};
+
+exports.checkForFull = function(module) {
+    if ( module['full-write-up'] !== undefined ) {
+        return module;
+    }
+};
