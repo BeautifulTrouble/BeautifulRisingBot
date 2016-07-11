c83ca09455de387bf72d3ee23916b351ace9b91f
Phillip Smith <ps@phillipadsmith.com>
2016-07-10 22:45:04 -0700

    Basic logging module (for unmatched commands)

---
+++ modules/logger/logger.js
@@ -0,0 +1,41 @@
+var winston = require('winston');
+var winstonCouch = require('winston-couchdb').Couchdb;
+// Configure the basic file logger
+winston.loggers.add('filelog', {
+    console: {
+        colorize: true,
+        label: 'log-file'
+    },
+    file: {
+        filename: './winston.log'
+    }
+});
+// Configure the couchdb logger
+winston.loggers.add('couchlog', {
+    console: {
+        colorize: true,
+        label: 'log-couch'
+    },
+    couchdb: {
+      host: 'localhost',
+      port: 5984
+    }
+});
+var couchlog = winston.loggers.get('couchlog');
+var filelog = winston.loggers.get('filelog');
+
+exports.match = function() {
+    return true;
+};
+
+// I intend to make this optional in future in case it doesn't actually respond to commands
+exports.help = function() {
+    return [];
+};
+
+exports.run = function(api, event) {
+        var command = api.commandPrefix;
+        couchlog.info('Received %s from %s', event.arguments[0], event.sender_name, { "message_id": event.thread_id, "command":  event.arguments[0], "user": event.sender_name, "response": "No command matched" }); 
+    // This needs to pull from botflow and provide translations
+        api.sendMessage('Ooops, I’m not sure what module you’re talking about. Try finding a module first using the ' + command + 'menu or ' + command + 'search command.' , event.thread_id);
+};
